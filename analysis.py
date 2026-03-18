76b1cb40aaa8ebc0344f7654e65243321b03a4a4
Abhishek goswami <abhi.pvt2523@gmail.com>
2026-03-19 04:31:56 +0530

    Creating files

---
+++ analysis.py
@@ -0,0 +1,13 @@
+# importing liabraries
+# import warnings
+# warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# importing data 
+import pandas as pd
+
+df = pd.read_csv("insurance.csv")
+print(df.head())
