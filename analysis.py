53ef82c4d97e0d949c0cb1e1146e6c3ab1e4e3ac
Abhishek goswami <abhi.pvt2523@gmail.com>
2026-03-19 14:26:33 +0530

    new lines of code shows EDA and columns counts

--- analysis.py
+++ analysis.py
@@ -7,7 +7,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # importing data 
-import pandas as pd
 
 df = pd.read_csv("insurance.csv")
-print(df.head())
+print(df.head()) 
+
+# Prforming EDA
+print(f'\n shape of the data frame is {df.shape}\n')
+print(df.info())
+print(f'\n decriptiion of the given dataset\n {df.describe()}\n')
+# now check is there any null value(removing null value is part of data cleaning)
+print(df.isnull().sum())
+print(f'\n columns in given dataset:\n {df.columns}\n')
+# extracting numeric columns
+numeric_col=['age', 'bmi', 'children', 'charges']
+for col in numeric_col:
+    plt.figure(figsize=(8,6))
+    sns.histplot(df[col], kde=True, bins=20)
+    plt.title(f"Distribution of {col}")
+    plt.show()
+
+
